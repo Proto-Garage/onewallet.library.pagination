ce5cc56c7d619f9291a641ab5e7dd9f5b072c0dd
Roger Madjos <roger.madjos@gmail.com>
2019-06-13 14:59:09 +0800

    build: update pre-commit hook

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,29 +1,23 @@
 import { Model, Document } from 'mongoose';
-export default function retrievePage<TNode = object>(
-  model: Model<Document, {}>,
-  params: {
+export default function retrievePage<TNode = object>(model: Model<Document, {}>, params: {
     first?: number;
     after?: string;
     filter?: {
-      [key: string]: any;
+        [key: string]: any;
     };
-  },
-  options?: {
+}, options?: {
     cursorKey?: string;
     sortDirection?: 1 | -1;
-    transform?: <TDocument = Document>(
-      document: TDocument
-    ) => TNode | Promise<TNode>;
-  }
-): Promise<{
-  totalCount: number;
-  edges: {
-    cursor: string;
-    node: TNode;
-  }[];
-  pageInfo: {
-    endCursor: string | null;
-    hasNextPage: boolean;
-  };
+    transform?: <TDocument = Document>(document: TDocument) => TNode | Promise<TNode>;
+}): Promise<{
+    totalCount: number;
+    edges: {
+        cursor: string;
+        node: TNode;
+    }[];
+    pageInfo: {
+        endCursor: string | null;
+        hasNextPage: boolean;
+    };
 }>;
 //# sourceMappingURL=index.d.ts.map
